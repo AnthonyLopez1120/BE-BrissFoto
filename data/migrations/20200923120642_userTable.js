8fa70ce85b42c5d9dfec51e9261b4c4134ff15bd
Anthony Lopez <anthonybeconscious@gmail.com>
2020-09-23 13:30:11 -0500

    set up knex, added user table, added blog table

---
+++ data/migrations/20200923120642_userTable.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('user', tbl => {
+        tbl.increments();
+        tbl.text('first-name', 128)
+            .notNullable()
+            .unique();
+        tbl.text('last-name', 128)
+            .notNullable()
+            .unique();
+        tbl.text('user-name', 128)
+            .notNullable()
+            .unique();
+        tbl.text('password', 128)
+            .notNullable()
+            .unique();
+    })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExist('user');
+};
