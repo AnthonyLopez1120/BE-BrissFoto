7d453ced7dd03e2352b5e0f64e3fd2c56148e357
Anthony Lopez <anthonybeconscious@gmail.com>
2020-09-23 13:33:32 -0500

    migrated tables

--- data/migrations/20200923131653_blog.js
+++ data/migrations/20200923131653_blog.js
@@ -3,11 +3,11 @@ exports.up = function(knex) {
     return knex.schema.createTable('blog', tbl => {
         tbl.increments();
         tbl.string('blog-post', 2500);
-        tbl.interger('user-id')
+        tbl.integer('user-id')
             .unsigned()
             .notNullable()
             .references('id')
-            .inTable('user')
+            .inTable('user');
     })
   
 };
