8973e869db09c3ed753e9753b5cd956d29e1e59e
Anthony Lopez <anthonybeconscious@gmail.com>
2020-12-04 15:15:29 -0600

    updated blog router

--- api/server.js
+++ api/server.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const usersRouter = require('./user/user-router.js')
-const authRouter = require('./auth/auth-router.js')
+const usersRouter = require("./user/user-router.js");
+const authRouter = require("./auth/auth-router.js");
+const blogRouter = requre("/blog/blog-router.js");
 
-const server = express()
+const server = express();
 
-server.use(morgan('combined'))
+server.use(morgan("combined"));
 
-server.use(helmet())
-server.use(express.json())
-server.use(cors())
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
-server.use('/api/users', usersRouter)
-server.use('/api/auth', authRouter)
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/blog", blogRouter);
 
-server.get('/', (req, res) =>{
-    res.status(200).json({ message: 'The api is lit... '})
-})
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "The api is lit... " });
+});
 
-module.exports = server
+module.exports = server;
